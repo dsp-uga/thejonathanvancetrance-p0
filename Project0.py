03be8181eb1b519db4fb125ef8eae894fe87a8a7
thejonathanvancetrance <thejonathanvance@gmail.com>
2021-01-26 14:02:35 -0500

    update code

--- Project0.py
+++ Project0.py
@@ -1,24 +1,72 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
 import dask.bag as db
+import jsonpickle
 
 
-b = db.read_text(['C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\pg36.txt',
-                 'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\4300-0.txt',
-                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\pg514.txt',
-                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\pg1497.txt',
-                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\pg3207.txt',
-                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\pg6130.txt',
-                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\pg19033.txt',
-                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\handout\\data\\pg42671.txt']).filter(lambda x: len(str(x)) >= 1)
+# In[7]:
 
+
+b = db.read_text(['C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\pg36.txt',
+                 'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\4300-0.txt',
+                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\pg514.txt',
+                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\pg1497.txt',
+                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\pg3207.txt',
+                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\pg6130.txt',
+                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\pg19033.txt',
+                'C:\\Users\\thejo\\Documents\\school\\CSCI8360 Data Practicum\\P0\\handout\\data\\pg42671.txt']).filter(lambda x: len(str(x)) >= 1)
+
+
+# In[8]:
 
 
 freqs = b.str.lower().frequencies(sort=True).topk(40, key=1)
-freqs.compute()
+out = freqs.compute()
 
 
+# In[9]:
+
+
+pickle = jsonpickle.encode(out)
+f = open("sp1.json", "w")
+f.write(pickle)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
 
 
